6d97edc9dcdae787d2ccb6e7acb8a714d0bc06ff
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-12 11:39:09 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            Console.WriteLine("inserisci il carattere");
+            a = Console.ReadLine();
+            foreach (char c in a)
+            {
+                Console.WriteLine((int)c);
+            }
+            Console.ReadLine();
+        }
+    }
+}
